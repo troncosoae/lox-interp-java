8793924251425d9faf5c7837402ddd6171e53fce
axel troncoso <atroncoso@tucar.app>
2024-08-16 17:33:36 -0400

    Finished chapter 6

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -55,9 +55,17 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
+
+        // for (Token token : tokens) {
+        //     System.out.println(token);
+        // }
 
     }
 
@@ -70,6 +78,14 @@ public class Lox {
         System.err.println(
             "[line" + line + "[ Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
 
