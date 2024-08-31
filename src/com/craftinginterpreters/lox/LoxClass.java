139d852923256057f885f0a09e3675553c668800
axel troncoso <atroncoso@tucar.app>
2024-08-30 21:05:24 -0400

    finished 12.5. methods in classes work now

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -1,12 +1,15 @@
 package com.craftinginterpreters.lox;
 
 import java.util.List;
+import java.util.Map;
 
 class LoxClass implements LoxCallable{
     final String name;
+    private final Map<String, LoxFunction> methods;
 
-    LoxClass(String name) {
+    LoxClass(String name, Map<String, LoxFunction> methods) {
         this.name = name;
+        this.methods = methods;
     }
 
     @Override
@@ -18,6 +21,13 @@ class LoxClass implements LoxCallable{
     public Object call(Interpreter interpreter, List<Object> arguments) {
         LoxInstance instance = new LoxInstance(this);
         return instance;
+    }
+
+    LoxFunction findMethod(LoxInstance instance, String name) {
+        if (methods.containsKey(name)) {
+            return methods.get(name);
+        }
+        return null;
     }
 
     @Override
